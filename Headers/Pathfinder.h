de368fec3174863d870d74a06b18c31ca41d5837
joseaam2020 <joseaam2019@hotmail.com>
2021-05-22 11:29:13 -0600

    Creacion de los primeros archivos para algoritmo PathFinding

---
+++ Headers/Pathfinder.h
@@ -0,0 +1,14 @@
+#ifndef PATHFINDER_H_
+#define PATHFINDER_H_
+
+#include <iostream>
+
+class Pathfinder
+{
+    private:
+
+    public:
+    Pathfinder();
+};
+
+#endif /* PATHFINDER_H_*/
---
+++ Source/Pathfinder.cpp
@@ -0,0 +1,6 @@
+#include "Pathfinder.h"
+
+Pathfinder::Pathfinder(){}
+
+
+
