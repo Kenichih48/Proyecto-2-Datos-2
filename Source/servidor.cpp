4715a61597567efbc7235451604b4125f793d035
Quigonar <67380073+Quigonar@users.noreply.github.com>
2021-05-30 18:17:58 -0600

    Se anaden los sources del Genetic Puzzle

---
+++ Source/servidor.cpp
@@ -0,0 +1,33 @@
+#include <SFML/Network.hpp>
+#include "../Headers/GeneticAlgorithm.h"
+#include <iostream>
+
+using namespace sf;
+using namespace std;
+
+///Main program
+int main(int argc, char const *argv[])
+{
+    IpAddress ip = IpAddress::getLocalAddress();
+    TcpSocket socket;
+    TcpListener listener;
+    listener.listen(8080);
+    listener.accept(socket);
+
+    Packet packetS, packetR;
+    string image;
+    int numPieces = 0;
+    bool primo = false;
+
+    while (true)
+    {
+        socket.receive(packetR);
+        if (packetR.getData() != NULL)
+        {
+            packetR >> image >> numPieces >> primo;
+            cout << image << " - " << numPieces << endl;
+            break;
+        }
+    }
+    GeneticAlgorithm solver(image, numPieces, primo);
+}
