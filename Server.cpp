2100cddc373fdc12e75c030835b899165fe22d2a
kenichih48 <kenichih48@yahoo.com>
2021-06-02 02:53:00 -0600

    backtracking fully operational

--- Server.cpp
+++ Server.cpp
@@ -9,6 +9,7 @@
 MatrixBP generateCleanMatrix();
 MatrixBP generatePlayingfield(int players);
 int random_num();
+bool generateSolution_aux(MatrixBP maze, int x, int y, MatrixBP sol);
 
 int main(){
 
@@ -33,9 +34,9 @@ int main(){
         }
     }
     
-    MatrixBP matrix = MatrixBP();
-    matrix = generateCleanMatrix();
-    matrix.print();
+    MatrixBP sol = MatrixBP();
+    sol = generateCleanMatrix();
+    sol.print();
 
     std::cout << std::endl;
 
@@ -43,6 +44,8 @@ int main(){
     maze = generatePlayingfield(numberPlayersInt);
     string mazeString = maze.print();
     std::cout << mazeString;
+
+    sol = generateSolution(maze, sol);
 
     packetS << mazeString;
     socket.send(packetS);
@@ -85,6 +88,7 @@ int main(){
     std::cout << "finished print" << std::endl;*/
 }
 
+//Función que genera las posiciones aleatorias iniciales de los jugadores
 MatrixBP generatePlayingfield(int players){
     MatrixBP matrix = MatrixBP();
 
@@ -103,12 +107,12 @@ MatrixBP generatePlayingfield(int players){
     int placed = 0;
     int decide;
     while(placed < players/2){
-        for(int x = 0; x <= 6;x++){
+        for(int x = 0; x <= 6; x++){
             for(int y = 0; y <= 2; y++){
-               decide = random_num();
-               if(decide == 50 && matrix.at(x)->at(y)->name == "0"){
+                decide = random_num();
+                if(decide == 50 && matrix.at(x)->at(y)->name == "0"){
                     matrix.at(x)->at(y)->name = "1";
-                    placed++;
+                    placed += 1;
                 }
             }
         }
@@ -120,7 +124,7 @@ MatrixBP generatePlayingfield(int players){
                 decide  = random_num();
                 if(decide == 50 && matrix.at(x)->at(y)->name == "0"){
                     matrix.at(x)->at(y)->name = "1";
-                    placed++;
+                    placed += 1;
                 }
             }
         }
@@ -129,22 +133,87 @@ MatrixBP generatePlayingfield(int players){
     return matrix;
 }
 
+//función que genera un int aleatorio entre 1 y 100
 int random_num(){
     int range = 100;
-    int random_int = (rand() % range)+1;
+    int random_int = (rand() % range) + 1;
     return random_int;
 }
 
+//función que genera una matriz con solamente 0's
 MatrixBP generateCleanMatrix(){
     MatrixBP matrix = MatrixBP();
 
     for(int i = 0; i <= 6; i++){
         ListBP list = ListBP();
-        for(int j = 0; j <= 6; j++){
+        for(int j = 0; j <= 7; j++){
             list.append("0");
         }
         matrix.append(list);
     }
 
     return matrix;
+}
+
+//función que verifica si un cuadro es libre y parte de la matriz
+bool check_safety(MatrixBP maze, int x, int y)
+{
+    if (x >= 0 && x < 7 && y >= 0 && y < 7 && maze.at(x)->at(y)->name == "0"){
+        return true;
+    }
+    return false;
+}
+ 
+//función que genera la ruta más corta entre la bola y el gol del jugador por backtracking
+MatrixBP generateSolution(MatrixBP maze, MatrixBP sol)
+{
+    if (generateSolution_aux(maze, 0, 0, sol) == false) {
+        printf("Solution doesn't exist");
+        return sol;
+    }
+    sol.print();
+    return sol;
+}
+ 
+//función auxiliar de generate solution, es la parte recursiva del backtracking
+bool generateSolution_aux(MatrixBP maze, int x, int y, MatrixBP sol)
+{
+    // caso gol
+    if (x == 3 && y == 0 && maze.at(x)->at(y)->name == "4") {
+        sol.at(x)->at(y)->name = "1";
+        return true;
+    }
+ 
+    if (check_safety(maze, x, y) == true) { 
+        if (sol.at(x)->at(y)->name == "1"){
+            return false;
+        }
+        sol.at(x)->at(y)->name = "1";
+ 
+        //nos movemos un bloque hacia abajo
+        if (generateSolution_aux(maze, x + 1, y, sol) == true){
+            return true;
+        }
+ 
+        //nos movemos un bloque hacia la derecha
+        if (generateSolution_aux(maze, x, y + 1, sol) == true){
+            return true;
+        }
+       
+        //nos movemos un bloque hacia arriba
+        if (generateSolution_aux(maze, x - 1, y, sol) == true){
+            return true;
+        }
+ 
+        //nos movemos un bloque hacia la izquierda
+        if (generateSolution_aux(maze, x, y - 1, sol) == true){
+            return true;
+        }
+ 
+        //si ninguna de las 4 direcciones sirve, significa que el bloque actual no pertenece a la solución
+        sol.at(x)->at(y)->name = "0";
+        return false;
+    }
+ 
+    return false;
 }
