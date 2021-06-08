a3665148d23661be1f498560850daf5191e6d9c3
Quigonar <67380073+Quigonar@users.noreply.github.com>
2021-06-08 02:25:45 -0600

    Se actualiza el nuevo cliente

--- Headers/geneticPuzzle.h
+++ Headers/geneticPuzzle.h
@@ -1,5 +1,5 @@
 #ifndef GENETICPUZZLE_H_
-#define GENETICPUZZLE_H_ 
+#define GENETICPUZZLE_H_
 
 
 #include <SFML/Network.hpp>
@@ -17,21 +17,115 @@ int geneticPuzzle() //int argc, char const *argv[]
     Packet packetS, packetR;
     int numPieces;
     bool primo = false;
-    string image;
+    string image, numPiecesStr, okStr;
     IpAddress ip = IpAddress::getLocalAddress();
     TcpSocket socket;
     ListGP<MatrixGP<int>*>* matrixes;
+    bool name, pieces;
+    Text imageText, numPiecesText, okText;
+    RectangleShape imageBackground, numPiecesBackground, background, ok;
+    Texture backgroundTexture;
+    Font font;
+    font.loadFromFile("Ubuntu-M.ttf");
+
+    imageText.setFont(font);
+    imageText.setCharacterSize(15);
+    imageText.setFillColor(Color::White);
+    imageText.setPosition(30,70);
+    imageBackground.setFillColor(Color::Black);
+    imageBackground.setSize(Vector2f(370,50));
+    imageBackground.setPosition(25,90);
+
+    numPiecesText.setFont(font);
+    numPiecesText.setCharacterSize(15);
+    numPiecesText.setFillColor(Color::White);
+    numPiecesText.setPosition(30,200);
+    numPiecesBackground.setFillColor(Color::Black);
+    numPiecesBackground.setSize(Vector2f(370,50));
+    numPiecesBackground.setPosition(25,220);
+
+    okStr = "DONE";
+    okText.setString(okStr);
+    okText.setFont(font);
+    okText.setCharacterSize(30);
+    okText.setFillColor(Color::Black);
+    okText.setPosition(170,330);
+    ok.setFillColor(Color::Green);
+    ok.setSize(Vector2f(150,50));
+    ok.setPosition(135,320);
+
+    backgroundTexture.loadFromFile("Background.png");
+    background.setPosition(0,0);
+    background.setTexture(&backgroundTexture);
+    background.setSize(Vector2f(420, 627));
 
     socket.connect(ip, 8080);
 
-    packetS << 1;
-    socket.send(packetS);
+    RenderWindow window(VideoMode(420,627,32), "Genetic Puzzle");
+    window.setVerticalSyncEnabled(true);
+    font.loadFromFile("Ubuntu-M.ttf");
 
-    cout << "Genetic Puzzle\n" << endl;
-    cout << "Please enter the name of the image file: " << endl;
-    cin >> image;
-    cout << "Enter the amount of pieces for the puzzle:" << endl;
-    cin >> numPieces;
+    while(window.isOpen())
+    {
+        sf::Event event;
+        while(window.pollEvent(event))
+        {
+            if (event.type == Event::MouseButtonPressed) {
+                if (event.mouseButton.button == Mouse::Left) {
+                    if (imageBackground.getGlobalBounds().contains(Vector2f(event.mouseButton.x, event.mouseButton.y)) && !name){
+                        name = true;
+                        pieces = false;
+                    }
+                    else if (numPiecesBackground.getGlobalBounds().contains(Vector2f(event.mouseButton.x, event.mouseButton.y)) && !pieces){
+                        pieces = true;
+                        name = false;
+                    }
+                    else if (ok.getGlobalBounds().contains(Vector2f(event.mouseButton.x, event.mouseButton.y)))
+                        window.close();
+                }
+            }
+            if (event.type == Event::Closed) {
+                window.close();
+                break;
+            }
+            if (event.type == Event::TextEntered) {
+                if (name) {
+                    if (event.text.unicode == 13)
+                        name = false;
+                    else if (event.text.unicode != 8)
+                        image += event.text.unicode;
+                    else {
+                        if (image.length() > 0)
+                            image.pop_back();
+                    }
+                } else if (pieces) {
+                    if (event.text.unicode == 13)
+                        pieces = false;
+                    else if (event.text.unicode != 8)
+                        numPiecesStr += event.text.unicode;
+                    else {
+                        if (numPiecesStr.length() > 0)
+                            numPiecesStr.pop_back();
+                    }
+                }
+            }
+        }
+
+        imageText.setString("Please enter the name of the image file:\n\n        " + image);
+        numPiecesText.setString("Enter the amount of pieces for the puzzle:\n\n                                         "
+                                + numPiecesStr);
+
+        window.draw(background);
+        window.draw(imageBackground);
+        window.draw(numPiecesBackground);
+        window.draw(ok);
+        window.draw(okText);
+        window.draw(imageText);
+        window.draw(numPiecesText);
+        window.display();
+
+    }
+
 
     while (!primo) {
         int divider = 1, dividers = 0;
@@ -46,7 +140,7 @@ int geneticPuzzle() //int argc, char const *argv[]
         }
         break;
     }
-    packetS.clear();
+    numPieces = stoi(numPiecesStr);
     packetS << image << numPieces << primo;
     socket.send(packetS);
 
